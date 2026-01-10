f7a0fefd29beee5202bbe0a5707137d85a5a47bf
Ozgur Topkan <upgunagent@gmail.com>
2026-01-10 21:22:52 +0300

    Security: Remove hardcoded API key

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 
 // Use provided key if env not set for dev, but strongly prefer env
-const resendApiKey = process.env.RESEND_API_KEY || 're_PP48TmTB_BkM6MYPBdfho73AUACYQiNtQ';
+const resendApiKey = process.env.RESEND_API_KEY!;
 
 export const resend = new Resend(resendApiKey);
 
